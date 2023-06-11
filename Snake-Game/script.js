f63c2c6ff96ac2754d2849496f22fea0127637df
Pedro-HFelix <pf711363@gmail.com>
2023-06-11 13:09:28 -0300

    Second Commit

--- Snake-Game/script.js
+++ Snake-Game/script.js
@@ -1,10 +1,23 @@
 let canvas =document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake =[];
+snake[0] = {
+    x: 8 * box,
+    y:8*box
+}
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16*box,16*box);
 }
+function criarCobra(){
+    for (let index = 0; index < snake.length; index++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[index].x,snake[index].y,box,box);
+        
+    }
+}
 criarBG();
+criarCobra();
 
   
